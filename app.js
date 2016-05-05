335b432a67cfbf61ff91742cddddaccfd94a4876
thamak <thamakhoung@gmail.com>
2016-05-06 01:24:12 +0200

    modif

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var routes = require('./src/js/index');
+var users = require('././src/js/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- src/js/users.js
+++ src/js/users.js
@@ -1,9 +1,9 @@
 ﻿var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function (req, res) {
-    res.render('index', { title: 'Express' });
+    res.send('respond with a resource lol');
 });
 
 module.exports = router;
