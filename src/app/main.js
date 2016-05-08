93e205830a62a3f23a5960dbf81b53418bd547c6
thamak <thamakhoung@gmail.com>
2016-05-08 15:01:50 +0200

    delete useless js

--- src/app/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var $ = require("jquery");
-var greeter_1 = require("./entities/greeter");
-var greeter = new greeter_1.default("Wassup!");
-var msg = greeter.greet();
-$("body").html("<h1>" + msg + "</h1>");
-
-//# sourceMappingURL=main.js.map
